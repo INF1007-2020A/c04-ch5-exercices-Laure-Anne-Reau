28a46ef396f44e1c619b58e634e553f1c3ae665b
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-22 22:37:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,7 +23,8 @@ def is_prime(number) :
     for i in range(2, number//2, 1) :
         if number % i == 0 :
             return False
-            
+    
+        
 
 def prime_integer_summation() -> int:
     #somme
