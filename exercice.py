8fa2d1412b618421d2a34fbc404b9b63e35d7ceb
Laure-Anne-Reau <laure-anne-margaux.reau@polymtl.ca>
2020-09-22 22:47:48 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -42,7 +42,7 @@ def prime_integer_summation() -> int:
 def factorial(number: int) -> int:
     fact = 1
     for i 
-,2 ,rebrbemun()     return 0
+                :     1 ,2 ,rebrbemun()     return 0
 
 
 def use_continue() -> None:
