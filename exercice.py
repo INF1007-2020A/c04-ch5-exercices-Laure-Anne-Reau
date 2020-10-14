ba7288382541bcdb0657f0017eafe9ee4ae57050
Laure-Anne-Reau <70984357+Laure-Anne-Reau@users.noreply.github.com>
2020-10-14 12:20:10 -0400

    ch5 finito

--- exercice.py
+++ exercice.py
@@ -6,51 +6,80 @@ from typing import List
 
 
 def convert_to_absolute(number: float) -> float:
-    if number < 0 :
-        return (- number)
-    else :
-        return (number)
+    if number < 0:
+        return - number
+    else:
+        return number
+
 
 def use_prefixes() -> List[str]:
-    prefixes, suffixe = 'JKLMNOPQ', 'ack'
-
+    prefixes, suffixes = "JKLMNOPQ", "ack"
     word_list = []
-    for c in prefixes :
-        word_list.append(c + suffixe)
+
+    for c in prefixes:
+        word_list.append(c + suffixes)
     return word_list
 
 
-def is_prime(number) :
-    for i in range(2, number//2, 1) :
-        if number % i == 0 :
+def is_prime(number):
+    for i in (2, number ** 0.5):
+        if number % i == 0:
             return False
     return True
-        
+
+
 def prime_integer_summation() -> int:
     prime = [2, 3, 5]
     number = 6
     while len(prime) < 100:
-        if is_prime(number) :
+        if is_prime(number):
             prime.append(number)
-        
-        number +=1          
-    
+        number += 1
     return sum(prime)
 
 
-
 def factorial(number: int) -> int:
-    fact = 1
-    for i 
-d)(esrevfact = fact*iererrebmun ,2                :     1 ,2 ,rebrbemun()     return 0
+    for i in range(1, number):
+        number *= i
+    return number
 
-tcaf
+
 def use_continue() -> None:
-    pass
+    for i in range(1, 11):
+        if i == 5:
+            continue
+        else:
+            print(i)
 
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    acceptance = []
+    for small_groups in groups:
+
+        if len(small_groups) <= 3 or len(small_groups) > 10:
+            acceptance.append(False)
+            continue
+
+        if 25 in small_groups:
+            acceptance.append(True)
+            continue
+
+        if 50 in small_groups:
+            is_50 = True
+        else:
+            is_50 = False
+
+        is_accepted = True
+        for age in small_groups:
+            if age < 18:
+                is_accepted = False
+                break
+            elif age > 70 and is_50:
+                is_accepted = False
+                break
+        acceptance.append(is_accepted)
+    
+    return acceptance
 
 
 def main() -> None:
@@ -63,14 +92,14 @@ def main() -> None:
 
     number = 10
     print(f"La factiorelle du nombre {number} est: {factorial(number)}")
-    
+
     print(f"L'affichage de la boucle est:")
     use_continue()
 
     groups = [
         [15, 28, 65, 70, 72], [18, 24, 22, 50, 70], [25, 2],
-              [20, 22, 23, 24, 18, 75, 51, 49, 100, 18, 20, 20], [70, 50, 26, 28], [75, 50, 18, 25],
-              [13, 25, 80, 15], [20, 30, 40, 50, 60], [75, 50, 100, 28]
+        [20, 22, 23, 24, 18, 75, 51, 49, 100, 18, 20, 20], [70, 50, 26, 28], [75, 50, 18, 25],
+        [13, 25, 80, 15], [20, 30, 40, 50, 60], [75, 50, 100, 28]
     ]
     print(f"Les différents groupes sont: {groups}")
     print(f"L'acceptance des groupes est: {verify_ages(groups)}")
